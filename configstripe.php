efaf8e1943d093cfa070b0ac459ad58c99eb0f4d
SomnathM41i <somnath.malim46@gmail.com>
2026-07-31 11:09:00 +0530

    Remove secrets from config files and add gitignore

--- configstripe.php
+++ configstripe.php
@@ -1,7 +1,7 @@
 <?php  require('stripe-php-master/init.php');
-$Publishablekey ="pk_test_REPLACED";
+$Publishablekey ="pk_test_REPLACE_WITH_YOUR_PUBLISHABLE_KEY";
 
-$Secretkey="sk_test_REPLACED";
+$Secretkey="sk_test_REPLACE_WITH_YOUR_SECRET_KEY";
 \Stripe\Stripe::setApiKey($Secretkey);
 
 ?>
